24aeee654b4f0b5c7b87e9739ce8997226df7b7c
Petar <echonykrom@protonmail.com>
2022-02-11 02:01:17 +0200

    transaction atomicity

---
+++ part_2/transaction_atomicity/bank/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+import decimal
+
+from django.core.exceptions import ValidationError
+
+from .models import customer
+
+
+class Payment(forms.Form):
+    payor = forms.CharField(max_length=30)
+    payee = forms.CharField(max_length=30)
+    amount = forms.CharField(max_length=30)
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     sender_name = cleaned_data["payor"]
+    #     sender = customer.objects.get(name=sender_name)
+    #     funds_to_send = decimal.Decimal(cleaned_data["amount"])
+    #     if funds_to_send > sender.balance:
+    #         self.add_error('payor', "Insufficient funds.")
